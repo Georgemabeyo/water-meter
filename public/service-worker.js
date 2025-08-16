5070071ffa00a275a92896b90778ca0363ea9f02
Georgemabeyo <149334413+Georgemabeyo@users.noreply.github.com>
2025-08-16 13:32:22 +0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,27 @@
-const CACHE_NAME = "water-dashboard-v1";
-const urlsToCache = ["/", "/index.html", "/css/style.css", "/manifest.json"];
+const CACHE_NAME = "water-meter-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/css/style.css",
+    "/manifest.json"
+];
 
 self.addEventListener("install", event => {
+    console.log("[ServiceWorker] Install");
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
     );
+});
+
+self.addEventListener("activate", event => {
+    console.log("[ServiceWorker] Activate");
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
     );
 });
